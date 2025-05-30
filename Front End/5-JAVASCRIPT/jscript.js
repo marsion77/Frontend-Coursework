6bdc3895935d697283556eef6a7122cfeb4f0fcb
Marison <marison399@gmail.com>
2025-05-30 16:17:13 +0530

    frist commit

---
+++ Front End/5-JAVASCRIPT/jscript.js
@@ -0,0 +1,51 @@
+document.write("Welcome To My Webpage")
+
+window.alert("Page Loaded Succesfully")
+
+// document.getElementById("head").innerText = "Welcome to the Page"
+
+
+
+// Data Types
+
+// 1.Number
+// 2. String
+// 3. Boolean
+
+
+
+// Number
+let amount = 100
+console.log(amount, typeof amount);
+
+// String
+let name = "Marison"
+console.log(name, typeof name)
+
+// String
+var idnumber = "ID009"
+console.log(idnumber, typeof idnumber)
+
+var idnumber = "ID005"
+console.log(idnumber, typeof idnumber)
+
+
+let a = 1
+let b = 2
+console.log(a + b)
+
+let x = "007"
+let y = 2
+console.log(x + y)
+
+
+
+// Boolean
+
+var place = true
+console.log(typeof place)
+
+var place = false
+console.log(typeof place)
+
+
