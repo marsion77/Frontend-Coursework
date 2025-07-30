768fc8f6295f883b980f0a87d128db7b2ccc59da
marison77 <marison399@gmail.com>
2025-07-30 09:23:54 +0530

    first commit

---
+++ Front End/calci.js
@@ -0,0 +1,29 @@
+
+var a = document.getElementById("show")
+
+function display(val){
+a.value += val
+}
+
+function Clear(){
+a.value = ""
+}
+
+function elete(){
+a.value = a.value.slice(0,-1);
+}
+
+function none(){
+document.getElementById("div").style.display = "none"
+}
+
+function calculate(){
+try{
+    a.value = eval(a.value)
+}
+catch(error){
+alert("Invalid Input")
+console.log(error);
+
+}
+}
